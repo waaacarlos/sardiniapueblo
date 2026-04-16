e6033ecc0d7fcb4cf71d504b7676d219b2ef76bf
Carlo Pisano <pisancarlo@gmail.com>
2026-04-16 16:43:17 +0200

    Vercel cors

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ async def serve_spa(full_path: str):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://sardiniapueblo.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
