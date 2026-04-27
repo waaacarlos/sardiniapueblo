9ee6b9a80cef2d3bdefeec02f4f8d15385859438
waaacarlos <pisancarlo@gmail.com>
2026-04-28 00:32:41 +0200

    Aggiustamento classifica

--- webapp/frontend/src/pages/Ranked.jsx
+++ webapp/frontend/src/pages/Ranked.jsx
@@ -60,9 +60,9 @@ export default function Ranked({ playerId, playerData, onPublicNameSaved }) {
 
   const columns = [
     { id: "rank", label: "#", minWidth: 50, fontSize: "1.5em", align: "right" },
-    { id: "fullname", label: "Giocatore", minWidth: 150 },
+    { id: "public_name", label: "Giocatore", minWidth: 150 },
     // { id: "ach", label: "Obiettivi", minWidth: 50, align: "center" },
-    { id: "acc", label: "Accuratezza", minWidth: 50, align: "center" },
+    { id: "acc", label: "Acc.", minWidth: 50, align: "center" },
     {
       id: "points",
       label: "Punti",
